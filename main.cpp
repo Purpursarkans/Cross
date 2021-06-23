4a0e0cfa1522d7d306a9d4880f28cdfa6edb4537
igorek <goga.danilov10@gmail.com>
2021-06-23 16:11:43 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,15 +6,13 @@ int main()
 {
 	cout << "Hello world on linux" << endl;
 	cout << "Hello world on windows" << endl;
-<<<<<<< HEAD
 	
 	cout << "Pravka s windows 1" << endl;
 	cout << "Pravka s windows 2" << endl;
-=======
-
 	cout << "Pravka s windows 1" << endl;
+	
 	cout << "Pravka s linux 1" << endl;
->>>>>>> dbc04b8acc378b888382d6773846a0dde57e0683
+
 	
 	return 0;
 	
