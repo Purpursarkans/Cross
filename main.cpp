dbc04b8acc378b888382d6773846a0dde57e0683
Purpursarkans <goga.danilov10@gmail.com>
2021-06-23 12:08:34 +0000

    update

--- main.cpp
+++ main.cpp
@@ -6,13 +6,9 @@ int main()
 {
 	cout << "Hello world on linux" << endl;
 	cout << "Hello world on windows" << endl;
-<<<<<<< HEAD
 
-	cout << "Pravka s linuxa 1" << endl;
-=======
-	
-	cout << "Правка с виндовс 1" << endl;
->>>>>>> 2b64c850d09fee8c62fbdd96190ada36542a04e2
+	cout << "Pravka s windows 1" << endl;
+	cout << "Pravka s linux 1" << endl;
 	
 	return 0;
 	
