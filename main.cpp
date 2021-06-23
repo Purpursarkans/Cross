6d36267f79d3a026a3604c134e481964da863b9b
Purpursarkans <goga.danilov10@gmail.com>
2021-06-23 12:28:19 +0000

    update

--- main.cpp
+++ main.cpp
@@ -15,7 +15,8 @@ int main()
 	cout << "Pravka s linux 1" << endl;
 	cout << "Pravka s linux 2" << endl;
 	cout << "Pravka s linux 3" << endl;
-	
+	cout << "Pravka s linux 4" << endl;
+		
 	
 	return 0;
 	
