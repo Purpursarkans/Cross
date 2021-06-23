ac76094fe9600f9bd65de52aa99e2a1633e6f56d
Purpursarkans <goga.danilov10@gmail.com>
2021-06-23 16:27:21 +0400

    update

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ int main()
 	cout << "Pravka s windows 3" << endl;
 	cout << "Pravka s windows 4" << endl;
 	cout << "Pravka s windows 5" << endl;
+	cout << "Pravka s windows 6" << endl;
 	
 	cout << "Pravka s linux 1" << endl;
 	cout << "Pravka s linux 2" << endl;
