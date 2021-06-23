392367c901fd4d65724775dcd92d1713cb42bd8e
Purpursarkans <goga.danilov10@gmail.com>
2021-06-23 16:35:02 +0400

    update

--- main.cpp
+++ main.cpp
@@ -7,22 +7,6 @@ int main()
 	cout << "Hello world on linux" << endl;
 	cout << "Hello world on windows" << endl;
 	
-	cout << "Pravka s windows 1" << endl;
-	cout << "Pravka s windows 2" << endl;
-	cout << "Pravka s windows 3" << endl;
-	cout << "Pravka s windows 4" << endl;
-	cout << "Pravka s windows 5" << endl;
-	cout << "Pravka s windows 6" << endl;
-	cout << "Pravka s windows 7" << endl;
-	cout << "Pravka s windows 8" << endl;
-	
-	cout << "Pravka s linux 1" << endl;
-	cout << "Pravka s linux 2" << endl;
-	cout << "Pravka s linux 3" << endl;
-	cout << "Pravka s linux 4" << endl;
-	cout << "Pravka s linux 5" << endl;
-	cout << "Pravka s linux 6" << endl;
-	
 	return 0;
 	
 }
