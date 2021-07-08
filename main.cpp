1ba9c805d39eaffe578731a27106c084b1b038e6
igorek <goga.danilov10@gmail.com>
2021-07-08 13:42:10 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
-
 using namespace std;
+
+#define DEBUG 1
+#define PLATFORM "NULL"
+#define BIT 64
+
+#if defined (__x86_64__) || defined(__aarch64__)
+
+        #ifdef _WIN64
+                #define PLATFORM "Windows"
+        #elif __linux__
+                #define PLATFORM "Linux"
+        #endif
+#else
+        #define BIT 0
+        exit (1);
+#endif
 
 int main()
 {
-	#ifdef __x86_64__
+        #if DEBUG == 1
                 cout << "x64 detect" << endl;
-		#ifdef _WIN64
-			cout << "windows64 detect" << endl;
-		#endif
-		#ifdef __linux__ 
-			cout << "linux detect" << endl;
-		#endif
-	#else
-		cout << "need x64bit platform" << endl;
-		return 0;
-	#endif
-	
+                cout << PLATFORM << " detect" << endl;
+        #endif
+        #if BIT != 64
+                cout << "need x64-bit platform" << endl;
+        #endif
+        return 0;
 }
