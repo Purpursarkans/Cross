73b4b9caec94103e74a54fe6cbe87619863932f5
igorek <goga.danilov10@gmail.com>
2021-08-19 15:42:18 +0000

    Update and rename main.cpp to cross.cpp

---
+++ cross.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+#define DEBUG 1
+
+#if defined (__x86_64__) || defined(__aarch64__)
+
+    #ifdef _WIN64
+        #define PLATFORM "Windows"
+    #elif __linux__
+        #define PLATFORM "Linux"
+    #else
+        #define PLATFONR "Unknown"
+    #endif
+    #define BIT 64
+#else
+        #define BIT 0
+        exit (1);
+#endif
+
+void pause()
+{
+    #ifdef PWIN
+        system("pause");
+    #elif PLIN
+        system("read -n1 -r -p \"Press any key to continue...\" key");
+    #endif
+}
+
+int main()
+{
+    #if DEBUG == 1
+        cout << "x64 detect" << endl << PLATFORM << " detect" << endl;
+    #endif
+    #if BIT != 64
+        cout << "need x64-bit platform" << endl;
+    #endif
+    pause();
+    return 0;
+}
